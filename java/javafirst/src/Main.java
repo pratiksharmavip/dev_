f3c3a662514b13ad7eea7c0868ed5c105b9d0db0
pratik <pratiksharmavip@gmail.com>
2023-01-05 03:12:30 +0530

    h

--- java/javafirst/src/Main.java
+++ java/javafirst/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello hniiiiiiiild!");
+
+        int xx=12;
+        int xx1=23;
+        int xx2=34;
+        int xx3=45;
+        int xx4=56;
+        int xx5=67;
+        int xx6=78;
+        int xx7=89;
+        int xx8=91;
+        int xx9=99;
+        int xx10=101;
+        System.out.println(xx+xx1+xx2+xx3+xx4+xx5+xx6+xx7+xx8+xx9+xx10);
+
+
     }
 }
